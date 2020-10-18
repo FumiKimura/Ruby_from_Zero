fff6bbb53dae9dfea734be1451d9bd845b881274
Fumi496 <67512601+Fumi496@users.noreply.github.com>
2020-10-18 19:32:40 +0900

    Create chapter4.rb

---
+++ chapter4.rb
@@ -0,0 +1,28 @@
+#4-1
+p ["コーヒー", "カフェラテ"]
+
+#4-2
+drinks = ["コーヒー", "カフェラテ", "モカ"]
+p drinks[1]
+p drinks.first
+p drinks.last
+
+#4-3
+p ["コーヒー", "カフェラテ"].push("モカ")
+p [2,3].unshift(1)
+p [1,2] + [3,4]
+
+#4-4
+p ["ティーラテ", "カフェラテ", "抹茶ラテ"]
+
+["ティーラテ", "カフェラテ", "抹茶ラテ"].each do |drink|
+  puts drink + "お願いします"
+end
+
+sum = 0
+[1,2,3].each do |x|
+  sum += x
+end
+p sum
+
+p []
